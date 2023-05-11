1cd42dea0bee7cc6da0f9b12dc7b3d1702f008f0
이상철 <zlrz6706@gmail.com>
2023-05-12 06:06:20 +0900

    Static Routing Practice

---
+++ src/app/contact/user/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserPage = () => {
+  return <h1>this is user page</h1>;
+};
+
+export default UserPage;
---
+++ src/app/help/mind/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MindPage = () => {
+  return <h1>This is mind page</h1>;
+};
+
+export default MindPage;
